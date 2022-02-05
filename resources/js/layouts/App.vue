a44a3002a432cf2dd3ee9640ef3ba87f2cf13b7c
Ridha Ahmad Firdaus <ridhaaf@gmail.com>
2022-02-05 23:31:23 +0700

    Update App.vue

---
+++ resources/js/layouts/App.vue
@@ -0,0 +1,59 @@
+<template>
+    <div>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark shadow-sm">
+            <div class="container-fluid">
+                <router-link :to="{ name: 'home' }" class="navbar-brand">
+                    Laravel-Vue SPA
+                </router-link>
+                <button
+                    class="navbar-toggler"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent"
+                    aria-controls="navbarSupportedContent"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation"
+                >
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div
+                    class="collapse navbar-collapse"
+                    id="navbarSupportedContent"
+                >
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <router-link
+                                class="nav-link"
+                                data-toggle="collapse"
+                                :to="{ name: 'home' }"
+                                >Home
+                            </router-link>
+                        </li>
+                        <li class="nav-item">
+                            <router-link
+                                class="nav-link"
+                                data-toggle="collapse"
+                                :to="{ name: 'about' }"
+                                >About
+                            </router-link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+        <div class="container">
+            <router-view></router-view>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    watch: {
+        $route() {
+            $("#navbarSupportedContent").collapse("hide");
+        },
+    },
+};
+</script>
